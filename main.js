632686b9a90d9fd82f00f4433a0bca3c56eb58ab
parukoo <hrk.0u0x@gmail.com>
2020-07-31 08:32:57 +0900

    タスクを表示

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 {
   const addButton = document.getElementById('add-button');
   const inputTask = document.getElementById('input-task');
-  const taskList = [];
-  const task = {
+  const taskList = document.getElementById('task-list');
+  const tasks = [];
+  const newTask = {
     id: 1,
     comment: null,
     completed: false
@@ -14,14 +15,50 @@
 
   // タスクを追加
   function addTask() {
-    const value = inputTask.value.trim();
+    newTask.comment = inputTask.value.trim();
     inputTask.value = '';
-    if (value) {
-      taskList.push({
-        id: task.id++,
-        comment: value,
+    if (newTask.comment) {
+      tasks.push({
+        id: newTask.id++,
+        comment: newTask.comment,
         completed: false
       });
+      showTask();
     }
+  }
+
+  // タスクを表示
+  function showTask() {
+
+    // tbodyの中身を空にする
+    while (taskList.firstChild) {
+      taskList.removeChild(taskList.firstChild);
+    }
+
+    tasks.forEach( task => {
+      const tr = document.createElement('tr');
+
+      const idTd = document.createElement('td');
+      idTd.textContent = task.id;
+      tr.appendChild(idTd);
+
+      const commentTd = document.createElement('td');
+      commentTd.textContent = task.comment;
+      tr.appendChild(commentTd);
+
+      const statusButtonTd = document.createElement('td');
+      const statusButton = document.createElement('button');
+      statusButton.textContent = '作業中';
+      statusButtonTd.appendChild(statusButton);
+      tr.appendChild(statusButtonTd);
+
+      const deleteButtonTd = document.createElement('td');
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = '削除';
+      deleteButtonTd.appendChild(deleteButton);
+      tr.appendChild(deleteButtonTd);
+
+      taskList.appendChild(tr);
+    });
   }
 }
